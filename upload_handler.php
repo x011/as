e0544017635bcadfe85f48e62f9478b58b509a53
pedrolobito <pedrolobito@gmail.com>
2014-09-22 20:14:50 +0200

    ss4
    ss4

--- upload_handler.php
+++ upload_handler.php
@@ -2,4 +2,12 @@
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 var_dump($_FILES);
+
+$gs_name = $_FILES['uploaded_files']['tmp_name'];
+move_uploaded_file($gs_name, 'gs://aspy/new_file.txt');
+
+if (false == rename('gs://my_bucket/new_file.txt', 'gs://my_bucket/newname.txt', $ctx)) {
+  die('Could not rename.');
+}
+
 ?>

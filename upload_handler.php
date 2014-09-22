7c5b5e76e1a52b74a4740b262867ec418df96009
pedrolobito <pedrolobito@gmail.com>
2014-09-22 22:22:32 +0200

    fu

--- upload_handler.php
+++ upload_handler.php
@@ -3,7 +3,7 @@
 var_dump($_FILES);
 
 $gs_name = $_FILES['uploaded_files']['tmp_name'];
-move_uploaded_file($gs_name, 'gs://aspy/new_file.txt');
+move_uploaded_file($gs_name, 'gs://android-spy11.appspot.com/new_file.txt');
 
 echo "me";
 ?>

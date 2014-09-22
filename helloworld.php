6dad55bae5af90e20b126d50fc15591a29d43084
pedrolobito <pedrolobito@gmail.com>
2014-09-22 20:05:25 +0200

    dd
    dd

--- helloworld.php
+++ helloworld.php
@@ -1,2 +1,16 @@
 <?php
-  echo 'Hello, World!';
+  //echo 'Hello, World!';
+  
+require_once 'google/appengine/api/cloud_storage/CloudStorageTools.php';
+use google\appengine\api\cloud_storage\CloudStorageTools;
+
+$options = [ 'gs_bucket_name' => 'aspy' ];
+$upload_url = CloudStorageTools::createUploadUrl('/upload_handler.php', $options);
+
+
+?>
+<form action="<?php echo $upload_url?>" enctype="multipart/form-data" method="post">
+    Files to upload: <br>
+   <input type="file" name="uploaded_files" size="40">
+   <input type="submit" value="Send">
+</form>

ade3f1a325df20ccaafeffdf429fb1e5746f32b5
pedrolobito <pedrolobito@gmail.com>
2014-09-22 20:26:26 +0200

    ddd
    ddd

--- upload_handler.php
+++ upload_handler.php
@@ -1,6 +1,6 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
+//error_reporting(E_ALL);
+//ini_set('display_errors', '1');
 var_dump($_FILES);
 
 $gs_name = $_FILES['uploaded_files']['tmp_name'];

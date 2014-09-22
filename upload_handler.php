45e937851d004ef5c3bc89b6a4a509d3519947ef
pedrolobito <pedrolobito@gmail.com>
2014-09-22 23:19:36 +0200

    uh3

--- upload_handler.php
+++ upload_handler.php
@@ -1,16 +1,35 @@
 <?php
 
-var_dump($_FILES);
+//var_dump($_FILES);
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 require_once 'google/appengine/api/cloud_storage/CloudStorageTools.php';
 use google\appengine\api\cloud_storage\CloudStorageTools;
 
 $gs_name = $_FILES['uploaded_files']['tmp_name'];
-move_uploaded_file($gs_name, 'gs://android-spy11.appspot.com/new_file.txt');
-$public_url = CloudStorageTools::getPublicUrl('gs://android-spy11.appspot.com/new_file.txt', true);
+move_uploaded_file($gs_name, 'gs://android-spy11.appspot.com/new_file33.txt');
+
+
+	
+
+    $storage = new Google_Service_Storage_StorageObject();
+    $storage->setName('gs://android-spy11.appspot.com/new_file33.txt');
+    $storage->setAcl('public-read');
+           
+    /*$acl = new Google_Service_Storage_ObjectAccessControl();
+    $acl->setEntity('allUsers');
+    $acl->setRole('READER');
+    $acl->setBucket('<BUCKET-NAME>');
+    $acl->setObject('<OBJECT-NAME>');
+     
+    // $storage being a valid Google_Service_Storage instance
+    $response = $storage->objectAccessControls->insert('<BUCKET-NAME>', '<OBJECT-NAME>', $acl);*/
+
+
+
+$object_public_url = CloudStorageTools::getPublicUrl('gs://android-spy11.appspot.com/new_file33.txt', true);
 //$options = stream_context_create(['gs'=>['acl'=>'public-read']]);
-$object_public_url = CloudStorageTools::getPublicUrl($public_url, false);
+//$object_public_url = CloudStorageTools::getPublicUrl($public_url, false);
 
 header('Location:' .$object_public_url);
 

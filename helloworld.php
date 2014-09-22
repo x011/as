60eff213cd6e244233e9b6907ed5cbc7c5bfa64d
pedrolobito <pedrolobito@gmail.com>
2014-09-22 22:19:38 +0200

    bucket

--- helloworld.php
+++ helloworld.php
@@ -5,12 +5,12 @@ ini_set('display_errors', '1');
 require_once 'google/appengine/api/cloud_storage/CloudStorageTools.php';
 use google\appengine\api\cloud_storage\CloudStorageTools;
 
-$options = [ 'gs_bucket_name' => 'aspy' ];
+$options = [ 'gs_bucket_name' => 'android-spy11.appspot.com' ];
 $upload_url = CloudStorageTools::createUploadUrl('/upload_handler.php', $options);
 //$upload_url = CloudStorageTools::createUploadUrl('/upload_handler.php');
 //$upload_url = "http://android-spy11.appspot.com/upload_handler.php";
 
-file_put_contents('gs://aspy/hello.txt', 'Hello');
+file_put_contents('gs://android-spy11.appspot.com/hello.txt', 'Hello');
 
 ?>
 <form action="<?php echo $upload_url?>" enctype="multipart/form-data" method="post">

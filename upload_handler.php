332adc2e85f76da0542ef0167feb0a2286a53fcb
pedrolobito <pedrolobito@gmail.com>
2014-09-22 22:29:42 +0200

    uh

--- upload_handler.php
+++ upload_handler.php
@@ -1,9 +1,14 @@
 <?php
 
 var_dump($_FILES);
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+require_once 'google/appengine/api/cloud_storage/CloudStorageTools.php';
+use google\appengine\api\cloud_storage\CloudStorageTools;
 
 $gs_name = $_FILES['uploaded_files']['tmp_name'];
 move_uploaded_file($gs_name, 'gs://android-spy11.appspot.com/new_file.txt');
+$public_url = CloudStorageTools::getPublicUrl('gs://android-spy11.appspot.com/new_file.txt', true);
 
-echo "me";
+echo $public_url;
 ?>

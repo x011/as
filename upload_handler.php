d19582c28d5ca16ab4608289009d6a0f3a813672
pedrolobito <pedrolobito@gmail.com>
2014-09-22 20:19:02 +0200

    ss
    ss

--- upload_handler.php
+++ upload_handler.php
@@ -5,9 +5,9 @@ var_dump($_FILES);
 
 $gs_name = $_FILES['uploaded_files']['tmp_name'];
 move_uploaded_file($gs_name, 'gs://aspy/new_file.txt');
-
+/*
 if (false == rename('gs://aspy/new_file.txt', 'gs://aspy/newname.txt', $ctx)) {
   die('Could not rename.');
-}
+}*/
 
 ?>

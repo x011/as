0fabaa2d0993b6b9c2ee075b651489bc4e5bacc6
pedrolobito <pedrolobito@gmail.com>
2014-09-23 12:07:10 +0200

    uh10

--- upload_handler.php
+++ upload_handler.php
@@ -20,7 +20,7 @@ move_uploaded_file($gs_name, 'gs://my_bucket/new_file.txt');*/
 $options = array('gs'=>array('acl'=>'public-read','Content-Type' => $_FILES['userfile']['type']));
 $ctx = stream_context_create($options);
 //$saved = file_put_contents($fileName, $_FILES['userfile']['tmp_name'], 0, $ctx);
-echo $saved."<br>";
+//echo $saved."<br>";
 
 if (false == rename($_FILES['userfile']['tmp_name'], $fileName, $ctx)) {
   die('Could not rename.');

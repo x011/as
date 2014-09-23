354dda9d403700f78d2c79dd798999c35270b5ab
pedrolobito <pedrolobito@gmail.com>
2014-09-23 12:06:22 +0200

    uh9

--- upload_handler.php
+++ upload_handler.php
@@ -12,10 +12,20 @@ use google\appengine\api\cloud_storage\CloudStorageTools;
 $fileName = 'gs://android-spy11.appspot.com/'.$_FILES['userfile']['name'];
 echo $fileName."<br>";
 
+
+/*$gs_name = $_FILES['userfile']['tmp_name'];
+move_uploaded_file($gs_name, 'gs://my_bucket/new_file.txt');*/
+
+
 $options = array('gs'=>array('acl'=>'public-read','Content-Type' => $_FILES['userfile']['type']));
 $ctx = stream_context_create($options);
-$saved = file_put_contents($fileName, $_FILES['userfile']['tmp_name'], 0, $ctx);
+//$saved = file_put_contents($fileName, $_FILES['userfile']['tmp_name'], 0, $ctx);
 echo $saved."<br>";
+
+if (false == rename($_FILES['userfile']['tmp_name'], $fileName, $ctx)) {
+  die('Could not rename.');
+}
+
 
 //if(is_file($_FILES['userfile']['tmp_name']))
 
